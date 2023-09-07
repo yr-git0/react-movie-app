b5e78cd5388e381d17c2806552ef99a2e519469f
Yaerin Park <programmer94@naver.com>
2023-09-07 23:26:15 +0900

    BrowserRouter에 basename 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
 function App() {
   return (
-    <Router>
+    <Router basename={`${process.env.PUBLIC_URL}`}>
       <Routes>
-        {/* <Route path="/hello" element={<h1>Hello</h1>} /> */}
-        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
