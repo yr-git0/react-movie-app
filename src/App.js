75978f883812b92ab7f053c82623574142ef8abf
Yaerin Park <programmer94@naver.com>
2023-09-07 23:09:36 +0900

    BrowserRouter > HashRouter로 변경

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
@@ -8,10 +8,7 @@ function App() {
       <Routes>
         {/* <Route path="/hello" element={<h1>Hello</h1>} /> */}
         <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
-        <Route
-          path={`${process.env.PUBLIC_URL}/movie/:id`}
-          element={<Detail />}
-        />
+        <Route path="/movie/:id" element={<Detail />} />
       </Routes>
     </Router>
   );
