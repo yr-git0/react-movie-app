8faafea32c3becdba3c6312e376f2738274a855b
Yaerin Park <programmer94@naver.com>
2023-09-07 22:47:59 +0900

    gh-pages 배포 후 빈화면 오류 수정

--- src/App.js
+++ src/App.js
@@ -7,8 +7,11 @@ function App() {
     <Router>
       <Routes>
         {/* <Route path="/hello" element={<h1>Hello</h1>} /> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/movie/:id" element={<Detail />} />
+        <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/movie/:id`}
+          element={<Detail />}
+        />
       </Routes>
     </Router>
   );
